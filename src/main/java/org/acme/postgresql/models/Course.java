1b5816f85a682629e591880f3883c6e7d2d6508a
Mauricio Micali <Mauricio.Micali@ocean.ibm.com>
2022-10-26 18:01:09 -0500

    Clase Course y Class añadido contenido

--- src/main/java/org/acme/postgresql/models/Course.java
+++ src/main/java/org/acme/postgresql/models/Course.java
@@ -11,7 +11,26 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Course extends PanacheEntity{
-    
+    public int course_id;
+    public String course_name;
+    public String course_description;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "course", fetch = FetchType.EAGER)
     private List<Class> classes;
+
+    public String getCourse_Name() {
+        return this.course_name;
+    }
+
+    public void setCourseName(String name) {
+        this.course_name = name;
+    }
+
+    public String getCourse_Description() {
+        return this.course_description;
+    }
+
+    public void setCourse_Description(String description) {
+        this.course_description = description;
+    }
 }
